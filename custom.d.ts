e6aaf41b0b47a09ec8d85ca2006b2f9b40beb2a1
SorayaRibeiro <soraya.alr@gmail.com>
2021-04-25 14:57:17 -0300

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=96f26c95035b98db861a&state=b63e8b2b-b7d6-4044-949f-6d81d47d0263

---
+++ custom.d.ts
@@ -0,0 +1,4 @@
+declare module '*.png'{
+    const content: amy;
+    export default content;
+}
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+import wateringImg from '../assets/watering.png';
+
+export function Welcome(){
+    return(
+    <SafeAreaView style={styles.container}>
+        <Text>Gerencie suas plantas de forma fácil
+        </Text>
+
+        <Image source={wateringImg} />
+
+        <Text>
+        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+sempre que precisar.
+        </Text>
+
+        <TouchableOpacity>
+            <Text>
+                >
+            </Text>
+
+        </TouchableOpacity>
+
+    </SafeAreaView>
+    
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    }
+
+})
